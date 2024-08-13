ded3ea05d4f906fe1da82f1b2cd127e752c9efec
vishwalavanya <vishwalavanya04@gamil.com>
2024-08-13 16:07:56 +0000

    commit messge

---
+++ src/components/Contact/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const Contact = () => (
+  <div className="contact-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
+      alt="contact"
+      className="contact-img"
+    />
+    <h1 className="contact-heading">Contact</h1>
+    <div className="div2">
+    <img src="https://i.imgur.com/LTtlzWb.png" className="logo" alt="linkedIn Logo"/>
+    <h2 className="contact-heading">vishwa jaganathan</h2>
+    </div>
+  </div>
+)
+
+export default Contact
---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      className="profile-img"
+      src="https://i.imgur.com/kpYxCOU.png"
+      alt="profile"
+    />
+    <h1 className="user-name">vishwa</h1>
+    <p className="user-designation">Software developer at India</p>
+  </div>
+)
+
+export default UserInfo
